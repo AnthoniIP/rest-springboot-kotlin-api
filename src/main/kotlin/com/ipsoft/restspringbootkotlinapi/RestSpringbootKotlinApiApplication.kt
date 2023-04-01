3e3a0376faa18ea1186181b5825e3e571a346a68
Anthoni Ipiranga <anthoni.ipiranga@gmail.com>
2023-04-01 14:08:18 -0300

    First commit

---
+++ src/main/kotlin/com/ipsoft/restspringbootkotlinapi/RestSpringbootKotlinApiApplication.kt
@@ -0,0 +1,11 @@
+package com.ipsoft.restspringbootkotlinapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RestSpringbootKotlinApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<RestSpringbootKotlinApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/ipsoft/restspringbootkotlinapi/RestSpringbootKotlinApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ipsoft.restspringbootkotlinapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestSpringbootKotlinApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
